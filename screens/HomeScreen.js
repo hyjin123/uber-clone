df9f82b50c802b92b10ef2beffb6d9f8f17879af
Sean <seanhoyeonjin@gmail.com>
2022-10-29 00:20:45 -0400

    Implemented Google Autocomplete Library, stored origin location info in redux

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,15 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  // dispatch all the relevant information to the data layer in redux (information such as lat and long)
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -17,6 +24,38 @@ const HomeScreen = () => {
             uri: "https://links.papareact.com/gzs",
           }}
         />
+
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          minLength={2}
+          enablePoweredByContainer={false}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          placeholder="Where From?"
+          debounce={400}
+        />
+
         <NavOptions />
       </View>
     </SafeAreaView>
