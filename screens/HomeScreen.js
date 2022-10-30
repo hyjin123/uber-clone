59d0cad6bb6250a00e2679c510217986e97f08d0
Sean <seanhoyeonjin@gmail.com>
2022-10-30 16:46:48 -0400

    added some views to the navigate card component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,6 +6,7 @@ import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplet
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from "react-redux";
 import { setDestination, setOrigin } from "../slices/navSlice";
+import NavFavourites from "../components/NavFavourites";
 
 const HomeScreen = () => {
   // dispatch all the relevant information to the data layer in redux (information such as lat and long)
@@ -57,6 +58,7 @@ const HomeScreen = () => {
         />
 
         <NavOptions />
+        <NavFavourites />
       </View>
     </SafeAreaView>
   );
